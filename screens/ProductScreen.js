e3d25404b9fdac8dcdfe25f6f534d791003ebfb1
Watcharapa2150 <mo.watcharapa_st@tni.ac.th>
2023-02-08 12:26:40 +0700

    practice

--- screens/ProductScreen.js
+++ screens/ProductScreen.js
@@ -83,7 +83,10 @@ const ProductScreen = ({navigation}) => {
         <TouchableOpacity
         style={styles.addButtonStyle}
         onPress = {()=>{
-          navigation.navigate('Detail')
+          navigation.navigate('Detail',{
+            id : item.id,
+            title : item.title
+          })
         }}>
       <View style={{ flex: 1 }}>
         <View style={{ flex: 1, flexDirection: "row", margin: 5 }}>
